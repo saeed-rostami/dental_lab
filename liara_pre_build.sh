ef6ddb0df69611e21a5a0648c06f0209a610fa5e
saeedrostami <saeedrostami1991@gmail.com>
2024-12-07 17:26:30 +0330

    add docker

--- liara_pre_build.sh
+++
@@ -1,5 +0,0 @@
-apt-get update;
-apt-get install php8.3-dev  -y --allow-unauthenticated;
-pecl install mongodb;
-printf "extension=mongodb.so\n" > /etc/php/8.3/mods-available/mongodb.ini;
-phpenmod -v 8.3 mongodb;
